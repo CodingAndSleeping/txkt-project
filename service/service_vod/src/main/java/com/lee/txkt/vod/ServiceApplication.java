5ffed648ae000b7913664632967d1cc8a83a947b
Lizhitao <1162300237@qq.com>
2023-08-06 22:51:12 +0800

    上传头像

--- service/service_vod/src/main/java/com/lee/txkt/vod/ServiceApplication.java
+++ service/service_vod/src/main/java/com/lee/txkt/vod/ServiceApplication.java
@@ -1,11 +1,14 @@
 package com.lee.txkt.vod;
 
+import com.lee.txkt.vod.utils.ConstantPropertiesUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.lee")
+@EnableConfigurationProperties(ConstantPropertiesUtil.class)
 public class ServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
---
+++ service/service_vod/src/main/java/com/lee/txkt/vod/utils/ConstantPropertiesUtil.java
@@ -0,0 +1,34 @@
+package com.lee.txkt.vod.utils;
+
+import lombok.Data;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "tencent.cos.file") // 使用该注解进行配置注入时，需要加getter和setter方法
+public class ConstantPropertiesUtil implements InitializingBean {
+
+    private String region;
+
+    private String secretId;
+
+    private String secretKey;
+
+    private String bucketName;
+
+    public static String END_POINT;
+    public static String ACCESS_KEY_ID;
+    public static String ACCESS_KEY_SECRET;
+    public static String BUCKET_NAME;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        END_POINT = region;
+        ACCESS_KEY_ID = secretId;
+        ACCESS_KEY_SECRET = secretKey;
+        BUCKET_NAME = bucketName;
+    }
+}
